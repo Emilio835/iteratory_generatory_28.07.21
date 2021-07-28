3c2640758e62dd00002d58ac602e73c256c54729
Emil <emil.zarzecki@gmail.com>
2021-07-28 18:33:40 +0200

    rest

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 if __name__ == '__main__':
     a = [1, 2, 3, 4, 5]
-    for liczba in a:
-        print(liczba)
+    for index,liczba in enumerate(a):
+        print(f'Index: {index}, Wartość: {liczba}')
 
     a = 'Ala ma kota'
     for litera in a:
         print(litera)
 
-    a = {'imie': 'Adam', 'nazwisko': 'Kowalski'}
+    a = {'imie': 'Adam', 'nazwisko': 'Kowalski', 'wiek': 25}
     for item in a.values():
         print(item)
